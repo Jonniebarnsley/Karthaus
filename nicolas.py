30f31060da154b8abaeee413d5a564a2e9c2d63b
Nicolas Sartore <nsartore@wisc.edu>
2024-05-25 17:10:33 +0200

    add almost complete plot function

--- nicolas.py
+++ nicolas.py
@@ -1,5 +1,6 @@
 import xarray as xr
 import matplotlib.pyplot as plt
+import cartopy.crs as crs
 
 dataPath = "../Data/"
 
@@ -8,10 +9,10 @@ def getIsochrones(dataPath):
 	return d
 
 def getMF(dataPath):
-	return xr.open_dataset(dataPath + "GISModel/fixed-seasonality.nc")
+	return xr.open_dataset(dataPath + "GISModel/fixed-seasonality.nc").h
 
 def getMV(dataPath):
-	return xr.open_dataset(dataPath + "GISModel/variable-seasonality.nc")
+	return xr.open_dataset(dataPath + "GISModel/variable-seasonality.nc").h
 
 
 def getIsochronesOld(dataPath, name):
@@ -32,10 +33,27 @@ def getIsochronesOld(dataPath, name):
 	return d
 
 
-def plot():
+def plot(time):
+	fix = getMF(dataPath)
+	var = getMV(dataPath)
+	iso = getIsochrones(dataPath)
+	print(iso.isochrone.values.tolist())
+
+	fig = plt.figure(figsize=(16,9), constrained_layout=True)
+	gs = fig.add_gridspec(1, 3)
+	ax1 = fig.add_subplot(gs[0,0], projection=crs.NorthPolarStereo(central_longitude=360-45))
+	ax1.pcolormesh(fix.x1, fix.y1, fix.sel(time=-time), transform=crs.PlateCarree())
+	ax2 = fig.add_subplot(gs[0,1], projection=crs.NorthPolarStereo(central_longitude=360-45))
+	ax2.pcolormesh(var.x1, var.y1, var.sel(time=-time), transform=crs.PlateCarree())
+
+	ax3 = fig.add_subplot(gs[0,2], projection=crs.NorthPolarStereo(central_longitude=360-45))
+	ax3.pcolormesh(iso.lon, iso.lat, iso.isochrone, transform=crs.PlateCarree())
 
 
-d = getIsochrones(dataPath)
-print(d)
-plt.contourf(d.isochrone)
-plt.show()
+	fig.suptitle(f"Time = {time}")
+	plt.show()
+plot(200)
+# d = getIsochrones(dataPath)
+# print(d.isochrone.values.tolist())
+# plt.contourf(d.isochrone.values[10:,10:])
+# plt.show()
