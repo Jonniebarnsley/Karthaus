05cd04ee3ae44fdc4f39992990f7354dedd22bac
Nicolas Sartore <nsartore@wisc.edu>
2024-05-25 16:17:30 +0200

    Funcion to load isochrones without lat/lon

--- nicolas.py
+++ nicolas.py
@@ -1,18 +1,20 @@
 import xarray as xr
 import matplotlib.pyplot as plt
 
-
 dataPath = "../Data/"
-for i in ["very_low", "low", "mid", "high"]:
-	try:
-		dd = xr.open_dataset(dataPath + f"Isochrones/9.0-8.5_ka_{i}_confidence_isochrone.nc").isochrone
-		if "d" not in locals():
-			d = dd
-		else:
-			d = d + dd
-	except:
-		print(i, "fail")
-	
 
+def getIsochrones(dataPath, name):
+	for i, confidence in enumerate(["very_low", "low", "mid", "high"]):
+		try:
+			dd = xr.open_dataset(dataPath + f"Isochrones/{name}_ka_{confidence}_confidence_isochrone.nc").isochrone
+			if "d" not in locals():
+				d = dd
+			else:
+				d = d + dd*(i+1)
+		except:
+			print(f"Isochrones {name} does not have confidence {confidence}")
+	return d
+
+d = getIsochrones(dataPath, "9.0-8.5")
 plt.contourf(d)
 plt.show()
